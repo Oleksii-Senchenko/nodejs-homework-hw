46876e6592697756e6bac277355f4646f915b6aa
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-10-10 22:06:50 +0300

    added mongoose

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+const DB_HOST =
+  "mongodb+srv://Oleksii:GCs33CxuZde0m8Sx@cluster0.g6e9a8j.mongodb.net/contact_reader?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message), process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
