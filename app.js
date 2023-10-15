c68bdad4e758421983beb99db4fb5e081b49b5ea
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-10-15 17:05:15 +0300

    hw3

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const contactsRouter = require("./routes/api/contacts");
-dotenv.config()
+dotenv.config();
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -19,7 +19,7 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  res.status(err.status || 500).json({ message: err.message });
 });
 
 module.exports = app;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,18 +1,109 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
-const ctrl = require('../../controllers/contacts');
+const HttpError = require("../../helpers/HttpError");
+const Contact = require("../../models/contact");
+const router = express.Router();
+const { isValidObjectId } = require("mongoose");
+const { addSchema, updateFavoriteSchema } = require("../../schemas/contact");
 
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await Contact.find();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    if (!isValidObjectId(contactId)) {
+      throw HttpError(400, `${contactId} is not valid ID`);
+    }
+    const result = await Contact.findById(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/', ctrl.getAll)
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await Contact.create(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/:contactId', ctrl.getById)
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { contactId } = req.params;
+    if (!isValidObjectId(contactId)) {
+      throw HttpError(400, `${contactId} is not valid ID`);
+    }
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+      new: true,
+    });
+    // res.status(200).json(result);
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', ctrl.add)
+router.patch("/:contactId/favorite", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    if (!isValidObjectId(contactId)) {
+      throw HttpError(400, `${contactId} is not valid ID`);
+    }
 
-router.put('/:contactId', ctrl.updateById)
+    const { error } = updateFavoriteSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
 
-router.delete('/:contactId', ctrl.remove)
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+      new: true,
+    });
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    if (!isValidObjectId(contactId)) {
+      throw HttpError(400, `${contactId} is not valid ID`);
+    }
+
+    const result = await Contact.findByIdAndRemove(contactId);
+
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json({ message: "contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
